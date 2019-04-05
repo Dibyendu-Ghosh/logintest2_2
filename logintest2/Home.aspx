05c7a32087596028058379fe4363b1616610e5fd
Dibyendu Ghosh <dibyendu.ghosh@gamutinfosystems.com>
2019-04-05 20:54:24 +0530

    test2

---
+++ logintest2/Home.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="MyProject.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+           <h1>Welcome All</h1>
+           <label>UserName</label>
+           <asp:TextBox runat="server" ID="txtuser"></asp:TextBox>
+           <label>Password</label>
+           <asp:Textbox runat="server" ID="Pass" TextMode="Password"></asp:Textbox>
+           <asp:Button runat="server" ID="btnLogin" OnClick="btnLogin_Click" Text="Login" />
+        
+        </div>
+    </form>
+</body>
+</html>
---
+++ logintest2/Home.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyProject
+{
+    public partial class Home : System.Web.UI.Page
+    {
+       public string username="abc";
+
+       public string password = "123";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtuser.Text.ToString() == username && Pass.Text.ToString() == password)
+            {               
+                     Response.Redirect("Welcome.aspx");             
+
+            }
+        }
+    }
+}
